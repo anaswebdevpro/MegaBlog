2a569deaa4696ba03c27f8519c87b01f5b5987d3
anaswebdevpro <anaswebdev.pro@gmail.com>
2025-12-29 12:20:49 +0530

    Resolve merge conflict in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,15 @@
-import React, { useEffect, useState } from "react";
-import authService from "./appwrite/auth";
-import { useDispatch } from "react-redux";
-import { login, logout } from "./store/authSlice";
-import { Footer, Header } from "./Components";
-import { Outlet } from "react-router";
-
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import authservice from './appwrite/authservice';
 
 const App = () => {
-  const [loading, SetLoading] = useState(true);
+
+
+  const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
-
-
-  useEffect(() => {
-    authService.getCurrentUser().then((userData) => {
-      if (userData) {
-        dispatch(login({ userData }));
-      } else {
-        dispatch(logout());
-      }
-    })
-    .finally(() => {
-      SetLoading(false);
-    });
-  }, []);
-if(loading){
-  return <div>Loading...</div>
-
+  return (
+    <div>App</div>
+  )
 }
 
-return (<>
-<div className="min-h-screen flex flex-wrap content-between bg-gray-300">
-  <div className="w-full block">
-    <Header />
-   <main>
-    <Outlet />
-     
-   </main>
-    <Footer />
-
-  </div>
-</div>
-
-</>)
-
-};
-
-
-export default App;
+export default App
